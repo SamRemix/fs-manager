0a17bdb890576f71cd4919dc5b29992427e127bb
SamRemix <s_capron@hotmail.fr>
2023-03-24 00:44:51 +0100

    navbar & header sass refactoring

--- front_/src/components/Navbar/index.jsx
+++ front_/src/components/Navbar/index.jsx
@@ -3,35 +3,48 @@ import './styles.scss'
 import { memo } from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 
+import { links, authLinks } from './links'
+
 const Navbar = () => {
   const { pathname } = useLocation()
 
-  const links = [
-    { path: '/', name: 'Home' },
-    { path: '/my-files', name: 'My files' },
-    { path: '/settings', name: 'Settings' },
-  ]
+  // checks if links includes pathname and then returns 'focus' in the jsx
+  const exists = links => (
+    links.some(({ path }) => path === pathname)
+  )
+
+  // create a function that returns jsx to display links (used in header & navbar)
+  const displayLinks = links => (
+    links.map(({ id, path, name }) => (
+      <li className={path === pathname ? 'item-active' : 'item'} key={id}>
+        <NavLink to={path} className="link">
+          <p className="link-title">{name}</p>
+        </NavLink>
+      </li>
+    ))
+  )
 
   return (
     <>
-      <header>
-        <h1 className="title">
-          {'Cloud_remix'.split('_').map((letter, i) => (
-            <span key={i}>{letter}</span>
+      <header className="header">
+        <h1 className="header-title">
+          {'Cloud_remix'.split('_').map((word, i) => (
+            <span key={i}>{word}</span>
           ))}
         </h1>
+
+        <ul className="header-items">
+          {displayLinks(authLinks)}
+
+          {exists(authLinks) && <div className="focus" />}
+        </ul>
       </header>
 
       <nav className="navbar">
         <ul className="navbar-items">
-          {links.map(({ path, name }, i) => (
-            <li key={i} className={path === pathname ? 'item-active' : 'item'}>
-              <NavLink to={path} className="link">
-                <p className="link-title">{name}</p>
-              </NavLink>
-            </li>
-          ))}
-          <div className="focus" />
+          {displayLinks(links)}
+
+          {exists(links) && <div className="focus" />}
         </ul>
       </nav>
     </>
---
+++ front_/src/components/Navbar/links.js
@@ -0,0 +1,27 @@
+export const links = [
+  {
+    id: 1,
+    path: '/',
+    name: 'Home'
+  }, {
+    id: 2,
+    path: '/my-files',
+    name: 'My files'
+  }, {
+    id: 3,
+    path: '/settings',
+    name: 'Settings'
+  }
+]
+
+export const authLinks = [
+  {
+    id: 1,
+    path: '/sign-up',
+    name: 'Sign up'
+  }, {
+    id: 2,
+    path: '/log-in',
+    name: 'Log in'
+  }
+]
