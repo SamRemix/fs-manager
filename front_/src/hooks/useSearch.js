286d02c823abd143d6f733ca33e6c4e36baf40ac
SamRemix <s_capron@hotmail.fr>
2023-03-25 22:44:43 +0100

    comments useSearch

--- front_/src/hooks/useSearch.js
+++ front_/src/hooks/useSearch.js
@@ -4,6 +4,9 @@ const useSearch = () => {
   const [prefix, setPrefix] = useState('')
 
   const searchBy = string => (
+    // removes spaces from name, email & prefix,
+    // converts them to lowercase to have the same string format
+    // then, checks if name or email includes the input value (prefix)
     string.trim().toLowerCase().includes(
       prefix.trim().toLowerCase()
     )
